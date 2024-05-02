ce842a24e6ccaac0e043ec4af1d6618fcb96bd3f
HanaokaY <okayu5757@ezweb.ne.jp>
2024-05-02 17:53:42 +0900

    Productコントローラ作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,17 +14,17 @@ class ProductsController < ApplicationController
     def create
         @product = Product.new(product_params)
         if @product.save
-        render json: @product, status: :created, location: @product
+            render json: @product, status: :created, location: @product
         else
-        render json: @product.errors, status: :unprocessable_entity
+            render json: @product.errors, status: :unprocessable_entity
         end
     end
 
     def update
         if @product.update(product_params)
-        render json: @product
+            render json: @product
         else
-        render json: @product.errors, status: :unprocessable_entity
+            render json: @product.errors, status: :unprocessable_entity
         end
     end
 
