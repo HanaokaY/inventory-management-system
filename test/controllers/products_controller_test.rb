2628185cf81fed987009ccea2eef1edf17c62d7c
HanaokaY <okayu5757@ezweb.ne.jp>
2024-05-02 18:27:38 +0900

    テスト未完成

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,22 @@
-require "test_helper"
+# require 'test_helper'
 
-class ProductsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class ProductsControllerTest < ActionController::TestCase
+#   setup do
+#     @product = products(:one)
+#   end
+
+#   test "should get index" do
+#     get :index
+#     assert_response :success
+#     assert_not_nil assigns(:products)
+#   end
+
+#   test "should create product" do
+#     assert_difference('Product.count') do
+#       post :create, params: { product: { name: @product.name, description: @product.description, price: @product.price, sku: @product.sku } }
+#     end
+
+#     assert_redirected_to product_path(assigns(:product))
+#   end
+
+# end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,13 @@
-require "test_helper"
+# require 'test_helper'
 
-class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class ProductTest < ActiveSupport::TestCase
+#   test "should not save product without name" do
+#     product = Product.new
+#     assert_not product.save, "Saved the product without a name"
+#   end
+
+#   test "should save product with valid attributes" do
+#     product = Product.new(name: "Example Product", description: "Example description", price: 10.99, sku: "123456")
+#     assert product.save, "Failed to save the product with valid attributes"
+#   end
+# end
